096c363bdffc14c40b6edd2daf9dbd5d771cad48
Q <Qrules@users.noreply.github.com>
2015-12-02 16:04:11 -0600

    Create Mainframe.java

---
+++ Mainframe.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Mainframe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e = new Employee("Aeron");
+		Scanner scan = new Scanner(System.in);		
+		String priority;
+		
+		System.out.println("1: Add a task");
+		System.out.println("2: Get a task");
+		System.out.println("3: Mark task complete");
+		System.out.println("4: Display last task completed");
+		System.out.println("5: Return last task completed");
+		System.out.println("6: Display employee info");
+		System.out.println("7: Exit");
+		System.out.print("What's your choice? ");
+		int choice = scan.nextInt();
+		
+		switch(choice)
+		{
+		case 1:
+		{
+			System.out.print("Please enter task: ");
+			String task = scan.nextLine();
+			System.out.print("Is it high priority?(y/n) ");
+			priority = scan.next();
+			if(priority.equalsIgnoreCase("y"))
+				e.addTask(task, 2);
+			if(priority.equalsIgnoreCase("n"))
+				e.addTask(task, 1);
+		}
+		case 2:
+			
+		case 3:
+			
+		case 4:
+			
+		case 5:
+			
+		case 6:
+			
+		case 7:
+			System.exit(1);
+		}
+	}
+
+}

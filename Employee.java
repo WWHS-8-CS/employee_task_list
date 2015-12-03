1803640218ca67bdbe5b3de56d37753c8d8fa440
Q <Qrules@users.noreply.github.com>
2015-12-03 10:19:04 -0600

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -12,11 +12,40 @@ public class Employee{
 		completedTasks = new Stack();
 	}
 	
+	public String toString()
+	{
+		return "Name: " + this.name;
+	}
+	
 	public void addTask(String task, int priority)
 	{
 		if(priority == 1)
 			lowTaskList.enqueue(task);
 		if(priority == 2)
 			highTaskList.enqueue(task);
+	}
+	
+	public String dequeueTask()
+	{
+		if(!highTaskList.isEmpty()) 
+			return highTaskList.dequeue();
+		return lowTaskList.dequeue();
+	}
+	
+	public String popDoneTask()
+	{
+		return completedTasks.pop();
+	}
+	
+	public String peekDoneTask()
+	{
+		return completedTasks.peek();
+	}
+	
+	public String peekTask()
+	{
+		if(!highTaskList.isEmpty()) 
+			return highTaskList.peek();
+		return lowTaskList.peek();
 	}
 }

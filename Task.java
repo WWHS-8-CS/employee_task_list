890c461156ee5f1f2b74bf8e1808a430658f6fef
Q <Qrules@users.noreply.github.com>
2015-12-04 11:43:44 -0600

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,1 +1,15 @@
+public class Task{
 
+	private String description;
+	
+	public Task(String description)
+	{
+		this.description = description;
+	}
+	
+	public String returnTask()
+	{
+	  return description;
+	}
+	
+}

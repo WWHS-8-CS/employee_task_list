82369de7d7de2ec2c2ab4e5ea1848c37dfc494e1
Q <Qrules@users.noreply.github.com>
2015-12-04 11:45:27 -0600

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,26 +1,26 @@
 import java.util.*;
 public class Queue{
 
-	private List<String> tasks;
+	private List<Task> tasks;
 	
 	public Queue()
 	{
-		tasks = new ArrayList<String>();
+		tasks = new ArrayList<Task>();
 	}
 	
-	public void enqueue(String task)
+	public void enqueue(Task t)
 	{
-		tasks.add(task);
+		tasks.add(t);
 	}
 	
-	public String dequeue()
+	public Task dequeue()
 	{	
 		if(tasks.size() == 0)
 			throw new IllegalStateException("Can't dequeue from an empty queue.");
 		return tasks.remove(0);
 	}
 	
-	public String peek()
+	public Task peek()
 	{	
 		if(tasks.size() == 0)
 			throw new IllegalStateException("Can't peek at an empty queue.");

e0d2a26e638794d813eeb0c16253b99ec6190fbf
Q <Qrules@users.noreply.github.com>
2015-12-04 11:48:03 -0600

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,15 +1,22 @@
 public class Task{
 
 	private String description;
+	private int priority;
 	
-	public Task(String description)
+	public Task(String description, int priority)
 	{
 		this.description = description;
+		this.priority = priority;
 	}
 	
 	public String returnTask()
 	{
-	  return description;
+		return description;
+	}
+	
+	public int returnPriority()
+	{
+		return priority;
 	}
 	
 }
